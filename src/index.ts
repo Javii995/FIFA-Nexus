f418bbcd06f54ae3b17fcc87d941a58a7e52d34f
Malik Gaysumov <s148435@ap.be>
2025-05-26 14:13:14 +0000

    Onnodige/test files verwijderen

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import app from './app';
-
-// De app wordt al opgestart in app.ts na MongoDB connectie
-// Dit bestand is alleen nodig voor de startpunt van de applicatie
-
-// Als je TypeScript compileert met tsconfig.json, zal dit het hoofdbestand zijn dat wordt gestart
